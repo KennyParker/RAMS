554df23974da0ba617784b4837fcde180c43c92d
KennyParker <parkerkr2@vcu.edu>
2017-04-23 15:06:34 -0400

    consumeAll renamed

--- include/consumer.h
+++ include/consumer.h
@@ -3,4 +3,4 @@
 #define DEGR_OF (180/M_PI)*
 
 void convert(struct laser *point, struct angle *angle1, struct angle *angle2, int *x, int *y, int *z);
-void triInitialize( struct controlAll *all, struct laser *point, struct angle *angle1, struct angle *angle2, struct spectral *spectrum1, struct spectral *spectrum2, struct control *c);
+void triInitialize( struct consumeAll *all, struct laser *point, struct angle *angle1, struct angle *angle2, struct spectral *spectrum1, struct spectral *spectrum2, struct control *c);

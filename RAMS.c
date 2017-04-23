75b7a7009c40d6a1f53b28c89b3901b661421072
KennyParker <parkerkr2@vcu.edu>
2017-04-23 19:11:02 -0400

    global *mysql

--- RAMS.c
+++ RAMS.c
@@ -70,24 +70,14 @@ int main(int argc, char *argv[])
     usleep(3.0e6);
 
 
-    mysql_close(mysql);
+    //mysql_close(mysql);
 
 
     pthread_join(t[4], NULL);
-        printf("join 4\n");
-
     pthread_join(t[3], NULL);
-    printf("join 3\n");
-
     pthread_join(t[2], NULL);
-
-    printf("join 2\n");
     pthread_join(t[1], NULL);
-        printf("join 1\n");
-
     pthread_join(t[0], NULL);
-
-    printf("join 0\n");
 
     return 0;
 }

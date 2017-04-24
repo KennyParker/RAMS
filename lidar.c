616c6ae930eb08b5230f08b1d5a5046c559c4670
KennyParker <parkerkr2@vcu.edu>
2017-04-24 17:18:12 -0400

    laser test

--- lidar.c
+++ lidar.c
@@ -38,7 +38,11 @@ void* laserRangefinder(void *p)
         rangefinder.time = (now.tv_sec - *c->start_time) * 1000 + (now.tv_nsec) / 1.0e6 ;
         rangefinder.distance = lidar_read(init);
         
-        printf("laser: %d %c", i%10==0?'\n':'\r');
+        printf("laser: %d %c", rangefinder.distance, i%10==0?'\n':'\r');
+
+
+          //printf("y%f\t p%f\t r%f %c", aim.yaw, aim.pitch, aim.roll, step%10==0?'\n':'\r');
+
 
         if(LamportQueue_push(queue, (void*)&rangefinder) ) // all's well
             i++;

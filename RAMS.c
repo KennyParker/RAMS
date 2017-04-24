d357f1b034560dc385501cfb389f9efad31f784b
KennyParker <parkerkr2@vcu.edu>
2017-04-24 15:08:37 -0400

    spect question

--- RAMS.c
+++ RAMS.c
@@ -32,12 +32,13 @@ int main(int argc, char *argv[])
     struct control control;
     init_control( &control, argc, argv );
 
+    printf("controls set, EXP = %d \n", control.exposure);
+    printf("controls set, DEBUG = %s \n", control.DEBUG?"on":"off");
     if(control.DEBUG){
         printf("size S :%d\n", SIZE_S);
         printf("size A :%d\n", SIZE_A);
         printf("size L :%d\n", SIZE_L);
     }
-    printf("controls set, DEBUG = %s \n", control.DEBUG?"on":"off");
 
     struct LamportQueue lidar;
     LamportQueue_init( &lidar, LASER_TYPE );
